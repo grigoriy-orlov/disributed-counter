02f186373613c7987ffdf04c573ec5bf3d52f0ca
ares4322 <ares4322@gmail.com>
2014-05-15 00:09:17 +0400

    + add code to initiator App and build config to pom.xml

--- initiator/src/main/java/ru/ares4322/distributedcounter/initiator/App.java
+++ initiator/src/main/java/ru/ares4322/distributedcounter/initiator/App.java
@@ -1,7 +1,49 @@
 package ru.ares4322.distributedcounter.initiator;
 
+import com.google.inject.CreationException;
+import com.google.inject.Injector;
+import org.slf4j.Logger;
+import ru.ares4322.distributedcounter.common.*;
+
+import static com.google.inject.Guice.createInjector;
+import static com.google.inject.Stage.PRODUCTION;
+import static java.lang.System.exit;
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class App {
+
+	private static final Logger log = getLogger(App.class);
+
 	public static void main(String[] args) {
+
+		Injector injector = null;
+		try {
+			injector = createInjector(
+				PRODUCTION,
+				new ConfigModule(args),
+				new CliModule(),
+				new ConnectionPoolModule(),
+				new CounterReceiverModule(),
+				new CounterSenderModule()
+			);
+
+		} catch (CreationException e) {
+			if (e.getCause() instanceof StartParamsParserException) {
+				log.error("parsing params error:", e);
+			} else {
+				log.error("context creation error:", e);
+			}
+			exit(1);
+		}
+		ConnectionPool pool = injector.getInstance(ConnectionPool.class);
+		CounterReceiverService receiverService = injector.getInstance(CounterReceiverService.class);
+		CounterSenderService senderService = injector.getInstance(CounterSenderService.class);
+		CliCommandReaderService commandReaderService = injector.getInstance(CliCommandReaderService.class);
+
+		receiverService.startAsync().awaitRunning();
+		commandReaderService.startAsync().awaitRunning();
+		pool.init();
+		senderService.startAsync().awaitRunning();
 
 	}
 }
