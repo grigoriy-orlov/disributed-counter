789669f09df9818cd53fc4c59c4f1d5ca4d947b3
ares4322 <ares4322@gmail.com>
2014-06-04 22:33:45 +0400

    * fix int -> Packet

--- echo/src/main/java/ru/ares4322/distributedcounter/echo/QueueModule.java
+++ echo/src/main/java/ru/ares4322/distributedcounter/echo/QueueModule.java
@@ -2,6 +2,7 @@ package ru.ares4322.distributedcounter.echo;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import ru.ares4322.distributedcounter.common.domain.Packet;
 
 import javax.inject.Singleton;
 import java.util.concurrent.BlockingQueue;
@@ -17,21 +18,21 @@ public class QueueModule extends AbstractModule {
 	@Provides
 	@Singleton
 	@ReceiverToSenderQueue
-	public BlockingQueue<Integer> getReceiverToSenderQueue() {
+	public BlockingQueue<Packet> getReceiverToSenderQueue() {
 		return newArrayBlockingQueue(20000);
 	}
 
 	@Provides
 	@Singleton
 	@ReceiverToSorterQueue
-	public BlockingQueue<Integer> getReceiverSorterQueue() {
+	public BlockingQueue<Packet> getReceiverSorterQueue() {
 		return newArrayBlockingQueue(20000);
 	}
 
 	@Provides
 	@Singleton
 	@BlackHoleQueue
-	public BlockingQueue<Integer> getSenderSorterQueue() {
+	public BlockingQueue<Packet> getSenderSorterQueue() {
 		return new ru.ares4322.distributedcounter.common.util.BlackHoleQueue<>();
 	}
 
